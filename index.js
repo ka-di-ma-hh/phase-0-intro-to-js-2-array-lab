8e98f487dc4dd25484a626897a7445014f0edb0a
junne.wanja@student.moringaschool.com <junne.wanja@student.moringaschool.com>
2024-07-11 14:38:06 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution here!
+// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+const destructivelyAppendCat = () => {
+  return cats.push('Ralph')
+}
+const destructivelyPrependCat = () => {
+  return cats.unshift('Bob')
+}
+const destructivelyRemoveLastCat = () => {
+  return cats.pop()
+}
+const destructivelyRemoveFirstCat = () => {
+  return cats.shift()
+}
+const appendCat = (name) => {
+return[...cats,'Broom']
+}
+const prependCat = (name) => {
+  return ['Arnold',...cats]
+}
+const removeLastCat = () => {
+ return cats.slice(0,-1)
+}
+const removeFirstCat = () => {
+  return cats.slice(1)
+}
